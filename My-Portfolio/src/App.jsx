83d6e401add8b86828f031cdc662d35186c00ca6
Brendon.btd <brendontafadzwa19@gmail.com>
2025-10-26 21:54:44 +0200

    debugging

--- My-Portfolio/src/App.jsx
+++ My-Portfolio/src/App.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import Header from "./components/Header"
-import Hero from "./components/Hero"
-import About from "./components/About"
-import Skills from "./components/Skills"
-import Projects from "./components/Projects"
-import Contact from "./components/Contact"
+import React from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
-    <div className='bg-gray-900'>
-      <Header/>
-      <Hero/>
-      <About/>
-      <Skills/>
-      <Projects/>
-      <Contact/>
+    <div className="bg-gray-900">
+      <Header />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
